11db6c6319a48cb2c3445a87ab5760b21ef28b81
Leon French <leonfrench@gmail.com>
2026-01-11 16:49:24 -0500

    Deploy edit to my repo

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ git init
 git add .
 git commit -m 'push to gh-pages'
 ## Change the line below to deploy to your gh-pages
-git push --force git@github.com:anvaka/map-of-github.git main:gh-pages
+git push --force git@github.com:leonfrench/map-of-proteins.git main:gh-pages
 cd ../
